5d7d3836a47362774e38ee33026e508f62324c99
Ashwin-Codes <kunwarbhargava2000@gmail.com>
2022-12-23 21:00:33 +0530

    add and configure cors && move server dependencies in server folder

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 import express from "express";
+import cors from "cors";
+import corsConfig from "./configs/corsConfigs.js";
+
 const __PORT = 5000;
 const app = express();
 
 // Routes
 import signupRoute from "./routes/signupRoute.js";
+
+// Cors
+app.use(
+	cors({
+		origin: corsConfig.origin,
+	})
+);
 
 app.listen(__PORT, () => {
 	console.log(`Server running on port ${__PORT}`);
