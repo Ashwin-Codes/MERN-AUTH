f5cb17e5c7ed7714e96a46fa03e26b1595c5a27e
Ashwin-Codes <kunwarbhargava2000@gmail.com>
2022-12-23 22:22:12 +0530

    change npm scripts && add and configure .env && add model database && configure mongoose

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import corsConfig from "./configs/corsConfigs.js";
+import signupRoute from "./routes/signupRoute.js";
+import connectDatabase from "./configs/dbConf.js";
+import mongoose from "mongoose";
 
-const __PORT = 5000;
+const __PORT = process.env.PORT || 5000;
 const app = express();
 
-// Routes
-import signupRoute from "./routes/signupRoute.js";
+// Database
+connectDatabase();
+
+// Middlewares
+app.use(express.json());
 
 // Cors
 app.use(
@@ -15,6 +24,11 @@ app.use(
 	})
 );
 
-app.listen(__PORT, () => {
-	console.log(`Server running on port ${__PORT}`);
+// Routes
+app.use(signupRoute);
+
+mongoose.connection.once("open", () => {
+	app.listen(__PORT, () => {
+		console.log(`Server running on port ${__PORT}`);
+	});
 });
--- server/routes/signupRoute.js
+++ server/routes/signupRoute.js
@@ -3,6 +3,5 @@ const router = express.Router();
 
 // Controllers
 import signupController from "../controllers/signupController.js";
-
 router.post("/signup", signupController);
 export default router;
