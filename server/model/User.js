eb21e6d8168397fe19bd3c69ca6357db55372140
Ashwin-Codes <kunwarbhargava2000@gmail.com>
2022-12-23 22:31:57 +0530

    add User DB Schema

---
+++ server/model/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongooses";
+
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	refreshToken: {
+		type: String || null,
+		default: null,
+	},
+});
+
+export default mongoose.model("User", userSchema);
