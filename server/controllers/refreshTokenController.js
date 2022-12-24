79bc2a1cfed6b50ab629c5a0ea4610f98888ab53
Ashwin-Codes <kunwarbhargava2000@gmail.com>
2022-12-24 18:39:22 +0530

    add refresh token route && add refresh token controller

---
+++ server/controllers/refreshTokenController.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import User from "../model/User.js";
+
+async function refreshTokenController(req, res, next) {
+	const cookies = req.cookies;
+	if (!cookies?.jwt) return res.sendStatus(401); // Unauthorized
+
+	const refreshToken = cookies.jwt;
+
+	const user = await User.findOne({ refreshToken }).exec();
+	if (!user) return res.sendStatus(401); // Unauthorized
+
+	jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+		if (err || user.username !== decoded.username) return res.sendStatus(401); // Unauthorized
+
+		const accessToken = jwt.sign({ username: user.username }, process.env.ACCESS_TOKEN_SECRET, {
+			expiresIn: "15m",
+		});
+		res.json({ accessToken });
+	});
+}
+
+export default refreshTokenController;
--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import corsConfig from "./configs/corsConfigs.js";
 import connectDatabase from "./configs/dbConf.js";
 import mongoose from "mongoose";
 import signupRoute from "./routes/signupRoute.js";
 import loginRoute from "./routes/loginRoute.js";
+import refreshTokenRoute from "./routes/refreshTokenRoute.js";
 
 const __PORT = process.env.PORT || 5000;
 const app = express();
@@ -17,6 +19,7 @@ mongoose.set("strictQuery", true);
 connectDatabase();
 
 // Middlewares
+app.use(cookieParser());
 app.use(express.json());
 
 // Cors
@@ -29,6 +32,7 @@ app.use(
 // Routes
 app.use(signupRoute);
 app.use(loginRoute);
+app.use(refreshTokenRoute);
 
 mongoose.connection.once("open", () => {
 	app.listen(__PORT, () => {
